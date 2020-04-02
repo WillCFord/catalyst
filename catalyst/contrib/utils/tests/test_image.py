3be24d940537e0b303c12210c6765b3faa08a44c
Yauheni Kachan <19803638+bagxi@users.noreply.github.com>
2020-04-02 14:02:36 +0300

    move torchvision to catalyst[cv]

--- catalyst/contrib/utils/tests/test_image.py
+++ catalyst/contrib/utils/tests/test_image.py
@@ -1,10 +1,101 @@
+from typing import Sequence
+
 import numpy as np
 
 import torch
-from torchvision.transforms.functional import normalize, to_tensor
 
 from catalyst import utils
 from catalyst.contrib.utils.image import _IMAGENET_MEAN, _IMAGENET_STD
+
+# <------- taken from torchvision - https://github.com/pytorch/vision ------->
+
+
+def _is_numpy(img: np.ndarray) -> bool:
+    return isinstance(img, np.ndarray)
+
+
+def _is_numpy_image(img: np.ndarray) -> bool:
+    return img.ndim in {2, 3}
+
+
+def normalize(
+    tensor: torch.Tensor,
+    mean: Sequence[float] = _IMAGENET_MEAN,
+    std: Sequence[float] = _IMAGENET_STD,
+    inplace: bool = False,
+) -> torch.Tensor:
+    """Normalize a tensor image with mean and standard deviation.
+
+    .. note::
+        This transform acts out of place by default, i.e.,
+        it does not mutates the input tensor.
+
+    Args:
+        tensor (torch.Tensor): tensor image of size (C, H, W) to be normalized
+        mean (Sequence[float]): Sequence of means for each channel
+        std (Sequence[float]): Sequence of standard deviations for each channel
+        inplace(bool,optional): bool to make this operation inplace
+
+    Returns:
+        torch.Tensor: normalized Tensor image
+    """
+    if not torch.is_tensor(tensor):
+        raise TypeError(
+            f"tensor should be a torch tensor. Got {type(tensor)}."
+        )
+
+    if tensor.ndimension() != 3:
+        raise ValueError(
+            f"Expected tensor to be a tensor image of size (C, H, W)."
+            f"Got tensor.size() = {tensor.size()}"
+        )
+
+    if not inplace:
+        tensor = tensor.clone()
+
+    dtype = tensor.dtype
+    mean = torch.as_tensor(mean, dtype=dtype, device=tensor.device)
+    std = torch.as_tensor(std, dtype=dtype, device=tensor.device)
+    if (std == 0).any():
+        raise ValueError(
+            f"std evaluated to zero after conversion to {dtype},"
+            f" leading to division by zero."
+        )
+    if mean.ndim == 1:
+        mean = mean[:, None, None]
+    if std.ndim == 1:
+        std = std[:, None, None]
+    tensor.sub_(mean).div_(std)
+    return tensor
+
+
+def to_tensor(pic: np.ndarray) -> torch.Tensor:
+    """Convert a ``numpy.ndarray`` to tensor
+
+    Args:
+        pic (numpy.ndarray): image to be converted to tensor
+
+    Returns:
+        torch.Tensor: converted image
+    """
+    if not _is_numpy(pic):
+        raise TypeError(f"pic should be PIL Image or ndarray. Got {type(pic)}")
+    elif not _is_numpy_image(pic):
+        raise ValueError(
+            f"pic should be 2/3 dimensional. Got {pic.ndim} dimensions."
+        )
+
+    if pic.ndim == 2:
+        pic = pic[:, :, None]
+
+    img = torch.from_numpy(pic.transpose((2, 0, 1)))
+    # backward compatibility
+    if isinstance(img, torch.ByteTensor):
+        return img.float().div(255)
+    return img
+
+
+# <------- taken from torchvision - https://github.com/pytorch/vision ------->
 
 
 def test_imread():
--- catalyst/data/scripts/image2embedding.py
+++ catalyst/data/scripts/image2embedding.py
@@ -1,3 +1,4 @@
+from typing import Sequence
 import argparse
 from pathlib import Path
 
@@ -7,9 +8,9 @@ import pandas as pd
 from tqdm import tqdm
 
 import torch
-from torchvision import transforms
 
 from catalyst.contrib.models.cv import ResnetEncoder
+from catalyst.contrib.utils.image import _IMAGENET_MEAN, _IMAGENET_STD
 from catalyst.data import ImageReader
 from catalyst.dl import utils
 
@@ -17,9 +18,63 @@ cv2.setNumThreads(0)
 cv2.ocl.setUseOpenCL(False)
 
 IMG_SIZE = (224, 224)
-IMAGENET_NORM = transforms.Normalize(
-    (0.485, 0.456, 0.406), (0.229, 0.224, 0.225)
-)
+
+
+# <------- taken from torchvision - https://github.com/pytorch/vision ------->
+
+
+def normalize(
+    tensor: torch.Tensor,
+    mean: Sequence[float] = _IMAGENET_MEAN,
+    std: Sequence[float] = _IMAGENET_STD,
+    inplace: bool = False,
+) -> torch.Tensor:
+    """Normalize a tensor image with mean and standard deviation.
+
+    .. note::
+        This transform acts out of place by default, i.e.,
+        it does not mutates the input tensor.
+
+    Args:
+        tensor (torch.Tensor): tensor image of size (C, H, W) to be normalized
+        mean (Sequence[float]): Sequence of means for each channel
+        std (Sequence[float]): Sequence of standard deviations for each channel
+        inplace(bool,optional): bool to make this operation inplace
+
+    Returns:
+        torch.Tensor: normalized Tensor image
+    """
+    if not torch.is_tensor(tensor):
+        raise TypeError(
+            f"tensor should be a torch tensor. Got {type(tensor)}."
+        )
+
+    if tensor.ndimension() != 3:
+        raise ValueError(
+            f"Expected tensor to be a tensor image of size (C, H, W)."
+            f"Got tensor.size() = {tensor.size()}"
+        )
+
+    if not inplace:
+        tensor = tensor.clone()
+
+    dtype = tensor.dtype
+    mean = torch.as_tensor(mean, dtype=dtype, device=tensor.device)
+    std = torch.as_tensor(std, dtype=dtype, device=tensor.device)
+    if (std == 0).any():
+        raise ValueError(
+            f"std evaluated to zero after conversion to {dtype},"
+            f" leading to division by zero."
+        )
+    if mean.ndim == 1:
+        mean = mean[:, None, None]
+    if std.ndim == 1:
+        std = std[:, None, None]
+    tensor.sub_(mean).div_(std)
+    return tensor
+
+
+# <------- taken from torchvision - https://github.com/pytorch/vision ------->
 
 
 def dict_transformer(sample):
@@ -30,7 +85,7 @@ def dict_transformer(sample):
     # image = np.concatenate([np.expand_dims(image, -1)] * 3, axis=-1)
     image = cv2.resize(image, IMG_SIZE, interpolation=cv2.INTER_NEAREST)
     image = torch.from_numpy(image.astype(np.float32) / 255.0).permute(2, 0, 1)
-    image = IMAGENET_NORM(image)
+    image = normalize(image)
 
     sample["image"] = image
     return sample
